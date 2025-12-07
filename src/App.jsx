0a386ce3951e0f99bc66f09aaddc3e1397830a08
Mohan Raj <mohanrajmohan1725@gmail.com>
2025-12-07 19:41:02 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-900">
+      <div className="text-center">
+        <h1 className="text-4xl font-extrabold text-red-500 mb-4">Tailwind CSS Working 🎉</h1>
+        <div className="inline-block p-4 rounded bg-red-500 text-white">If this is red, Tailwind works</div>
+      </div>
+    </div>
+  );
+}
